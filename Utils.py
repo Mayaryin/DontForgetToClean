8812ddb6edd039a29c36cdcd49503e09026884b2
lynn-janzen007 <lynn.janzen@campus.tu-berlin.de>
2024-06-01 20:12:13 +0200

    refactoring, added error handler

---
+++ Utils.py
@@ -0,0 +1,66 @@
+import logging
+import re
+from typing import Text
+from datetime import datetime, timedelta
+
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+def log(message: Text):
+    logger.log(20, message)
+
+def log_error(message: Text):
+    logger.log(40, message)
+
+# Helper functions
+def parse_names(text: Text):
+    return [name.lower() for name in text.split(", ") if name]
+
+def convert_timer_settings(info) -> int:
+    day = weekday_to_int(info[0].lower())
+    interval, hour, minute = [extract_and_convert_to_int(info[i]) for i in range(1, 4)]
+    return day, interval, hour, minute
+
+def extract_and_convert_to_int(string):
+    match = re.search(r'\d+', string)
+    if match:
+        return int(match.group())
+    else:
+        raise ValueError("No number found in the string")
+
+def weekday_to_int(weekday_name):
+    weekday_name = weekday_name.lower()
+    weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+    if weekday_name in weekdays:
+        return weekdays.index(weekday_name)
+    else:
+        raise ValueError(f"{weekday_name} is not a valid weekday name")
+
+def validate_input(text: str):
+    pattern = re.compile(
+        r'^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday),\s*[0-9],\s*(0?[0-9]|1[0-9]|2[0-3]),\s*(0?[0-9]|[1-5][0-9])$')
+    if pattern.match(text):
+        return True
+    return False
+
+def determine_start_date(target_weekday, hour, minute, timezone):
+    now = datetime.now()
+    current_weekday = now.weekday()
+    days_until_next = (target_weekday - current_weekday + 7) % 7
+    start_date = now + timedelta(days=days_until_next)
+    start_date = start_date.replace(hour=hour, minute=minute, second=0, microsecond=0)
+    log(f"now: {now}\n"
+        f"target weekday: {target_weekday}\n"
+        f"current weekday: {current_weekday}\n"
+        f"days until next: {days_until_next}\n"
+        f"start date: {start_date}")
+    return start_date
+
+
--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-import logging
-import re
-from collections import deque
-
-from dotenv import load_dotenv
 import os
-from typing import Final, Text
+from dotenv import load_dotenv
 from tzlocal import get_localzone
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, \
     ConversationHandler
-from datetime import datetime, timedelta
 from apscheduler.triggers.interval import IntervalTrigger
 
 from CleaningSchedule import CleaningSchedule
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+from Utils import *
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -38,8 +26,6 @@ async def show_mitbewohnys_command(update: Update, context: ContextTypes.DEFAULT
     names = cleaning_schedule.schedule
     text = f"The list is empty, start adding mitbewohnys by typing /add_mitbewohny" if not names else ", ".join(names)
     await update.message.reply_text(text)
-
-
 
 # Conversations
 ADDING = range(1)
@@ -77,19 +63,19 @@ async def done_removing(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     return ConversationHandler.END
 
 async def done_setting_reminder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if not validate_input(update.message.text):
+        await update.message.reply_text("Please stick to the format. Example: Sunday, 1, 12, 30")
 
-    #TODO: clean up this integer string mess
     info = update.message.text.split(", ")
-    day = info[0].lower()
-    interval = info[1]
-    day_as_int, interval_as_int, hour_as_int, minute_as_int = parse_timer_settings(update.message.text)
-    chat_id = update.effective_message.chat_id
-    # TODO: make timezone adjustable
+    day_as_int, interval_as_int, hour_as_int, minute_as_int = convert_timer_settings(info)
     timezone = get_localzone()
-    context.job_queue.run_custom(reminder, job_kwargs={"trigger": IntervalTrigger(seconds=interval_as_int,
+    chat_id = update.effective_message.chat_id
+    context.job_queue.run_custom(reminder, job_kwargs={"trigger": IntervalTrigger(weeks=interval_as_int,
         start_date=determine_start_date(day_as_int, hour_as_int, minute_as_int, timezone))
     }, chat_id=chat_id)
 
+    day = info[0]
+    interval = info[1]
     text =f"Your reminder will be sent out on {day}s every week!" if interval == "1" \
         else f"Your reminder will be sent out on {day}s every {interval} weeks!"
 
@@ -105,52 +91,10 @@ async def reminder(context: ContextTypes.DEFAULT_TYPE) -> None:
     name = cleaning_schedule.get_next_person()
     await context.bot.send_message(context.job.chat_id, text=f"{name} has to clean the flat this week!")
 
-# Helper functions
-def parse_names(text: Text):
-    return [name.lower() for name in text.split(", ") if name]
+# Error
 
-def parse_timer_settings(text: Text) -> int:
-    info = text.split(", ")
-    day = weekday_to_int(info[0].lower())
-    interval = extract_and_convert_to_int(info[1])
-    hour = extract_and_convert_to_int(info[2])
-    minute = extract_and_convert_to_int(info[3])
-    return day, interval, hour, minute
-
-def extract_and_convert_to_int(string):
-    match = re.search(r'\d+', string)
-    if match:
-        return int(match.group())
-    else:
-        raise ValueError("No number found in the string")
-
-def weekday_to_int(weekday_name):
-    weekday_name = weekday_name.lower()
-    weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-
-    if weekday_name in weekdays:
-        return weekdays.index(weekday_name)
-    else:
-        raise ValueError(f"{weekday_name} is not a valid weekday name")
-
-
-def determine_start_date(target_weekday, hour, minute, timezone):
-    now = datetime.now()
-    current_weekday = now.weekday()
-    days_until_next = (target_weekday - current_weekday + 7) % 7
-    start_date = now + timedelta(days=days_until_next)
-    start_date = start_date.replace(hour=hour, minute=minute, second=0, microsecond=0)
-    log(f"now: {now}\n"
-        f"target weekday: {target_weekday}\n"
-        f"current weekday: {current_weekday}\n"
-        f"days until next: {days_until_next}\n"
-        f"start date: {start_date}")
-    return start_date
-
-
-
-def log(message: Text):
-    logger.log(20, message)
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log_error(f'Update {update.update_id} caused error: {context.error}')
 
 
 if __name__ == '__main__':
@@ -206,8 +150,8 @@ if __name__ == '__main__':
     app.add_handler(removing_conv_handler)
     app.add_handler(setting_reminder_conv_handler)
 
-    #TODO: add error handlers
-
+    # Errors
+    app.add_error_handler(error)
 
     # Run the bot
     log("Polling..")
